08e587e84c4284c908656f8652ba0072d30c9d48
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-18 16:10:43 -0600

    feat(): basic use of interfaces

---
+++ src/10-interfaces.ts
@@ -0,0 +1,30 @@
+type Sizes = 'S' | 'M' | 'L' | 'XL';
+// type Product = {
+//   id: string | number;
+//   title: string;
+//   createdAt: Date;
+//   stock: number;
+//   size?: Sizes;
+// }
+
+interface Product {
+  id: string | number;
+  title: string;
+  createdAt: Date;
+  stock: number;
+  size?: Sizes;
+}
+
+const products: Product[] = [];
+
+products.push({
+  id: 1,
+  title: 'Product 1',
+  createdAt: new Date(),
+  size: 'S',
+  stock: 0
+});
+
+const addProduct = (data: Product) => {
+  products.push(data);
+}
