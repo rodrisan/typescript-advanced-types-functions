f2315793c33e75b8b3b55b5a61510fb651d3f047
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-18 18:02:27 -0600

    feat(): use of readonly

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,5 +2,7 @@ import { Product } from './product.model'
 const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = '';
+  // data.createdAt = new Date(1980, 0, 1);
   products.push(data);
 }
