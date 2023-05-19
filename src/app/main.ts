622bd646d77b2d1ca6cc677838238fc6f6495ebd
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-18 18:37:36 -0600

    feat(): using faker

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,28 @@
-import { addProduct } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: 1,
-  title: 'Product 1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  size: 'S',
-  stock: 90,
-  category: {
-    id: 1,
-    name: 'Category',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }
-});
+import { addProduct, products } from './products/product.service';
+
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    color: faker.color.human(),
+    category: {
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    createdAt: faker.date.recent(),
+    desc: faker.commerce.productDescription(),
+    id: faker.string.uuid(),
+    image: faker.image.url(),
+    isNew: faker.datatype.boolean(),
+    price: faker.number.float({min: 100.00, max: 1000.00}),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    stock: faker.number.int({min: 1, max: 100}),
+    tags: [faker.commerce.department(), faker.commerce.department()],
+    title: faker.commerce.productName(),
+    updatedAt: faker.date.recent(),
+  });
+
+  console.log(products);
+}
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,25 @@
 import { Product } from './product.model'
-const products: Product[] = [];
+
+export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
   // data.id = '';
   // data.createdAt = new Date(1980, 0, 1);
   products.push(data);
+}
+
+export const getProduct = (id: number) => {
+  // code
+}
+
+export const updateProduct = (id: number | string, changes: Product) => {
+  // code
+}
+
+export const deleteProduct = (id: number) => {
+  // code
+}
+
+export const allProducts = () => {
+  // code
 }
